15aba9817c36abf30b82fb18b3f6a2132742d526
GKastet <127600582+GKastet@users.noreply.github.com>
2023-12-15 12:18:41 +0100

    router.push(or replace)

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,31 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.push("/product");// creates a new route in history, able fetch data
+    // router.replace("/product");//will replace the browser history, better for modal
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
-      <Link href='/blog'>
-      Blog
-      </Link>
-      <Link href='/product'>
-      Products
-      </Link>
-
+      <Link href="/blog">Blog</Link>
+      <Link href="/product">Products</Link>
+      <button type="button" onClick={handleClick}>
+        Place Order
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 // function Home(){
 //     return (
 //         <h1>Home</h1>
-//         ) 
+//         )
 //     }
-    
-export default Home
+
+export default Home;
